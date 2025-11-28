528b211b2acefd82a32cd7a8dc93b6fe221a8129
joakotopos <jo.sanhuezav@duocuc.cl>
2025-11-28 01:51:58 -0300

    agregado de supabase
    se agregaron las dependencias de supabase para kotlin. (supabase ya tiene la base de datos creada)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    // Supabase Kotlin client
+    implementation("io.github.jan-tennert.supabase:postgrest-kt:2.3.3")
+    implementation("io.github.jan-tennert.supabase:auth-kt:2.3.3")
+
+    // Ktor Client (requerido por Supabase)
+    implementation("io.ktor:ktor-client-okhttp:2.3.6")
+
 }
